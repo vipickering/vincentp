edbe42bed2987d1698146a2b37f42ff8f409385c
Vincent Pickering <hello@vincentp.me>
2016-11-13 11:35:00 +0000

    tweaked grid to be mobile first

--- gulp/shell.js
+++ gulp/shell.js
@@ -1,4 +1,4 @@
 var gulp  = require('gulp');
 var shell = require('gulp-shell');
 
-gulp.task('jekyll', shell.task('cd website && jekyll serve --watch --drafts'));
+gulp.task('jekyll', shell.task('cd website && jekyll serve --watch --drafts --incremental'));
