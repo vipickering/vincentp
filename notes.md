efcf4e5014a933cb165d48f495c24534c1a8e0a0
vipickering <hello@vincentp.me>
2017-06-21 08:05:28 +0100

    :construction: still restructuring

--- notes.md
+++ notes.md
@@ -1,8 +1,8 @@
 ---
 layout : "category"
-permalink : "/categories/notes/"
-title : "Notes"
-meta : "All content tagged Notes by Vincent Pickering"
-summary : "All content tagged Notes by Vincent Pickering"
+permalink : "/categories/tips/"
+title : "Tips"
+meta : "All content tagged Tips by Vincent Pickering"
+summary : "All content tagged Tips by Vincent Pickering"
 type : Notes
 ---
--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,11 @@
 module.exports = {
   map: false,
   plugins: [
-    require('postcss-import')({}),
-    require('stylelint')({}),
-    require('css-mqpacker')({}),
+    require('postcss-import'),
+    require('stylelint'),
+    require('postcss-css-variables'),
+    require('css-mqpacker'),
     require('autoprefixer')({'browsers': 'last 2 versions'}),
-    require('cssnano')({})
+    require('cssnano')
   ]
 }
