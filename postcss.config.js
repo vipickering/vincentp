8e47f5ac64dd09d6da437de0d690357403c93d9e
vipickering <hello@vincentp.me>
2017-06-24 19:54:53 +0100

    :construction: restructring whole site

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,6 @@ module.exports = {
   map: false,
   plugins: [
     require('postcss-import'),
-    require('stylelint'),
     require('postcss-css-variables'),
     require('css-mqpacker'),
     require('autoprefixer')({'browsers': 'last 2 versions'}),
