6a7210a698de67120697b471d747e8f65db4e8e5
vipickering <hello@vincentp.me>
2017-08-16 09:37:47 +0100

    :construction_worker: updaing postcss packages

--- postcss.config.js
+++ postcss.config.js
@@ -2,9 +2,9 @@ module.exports = {
   map: false,
   plugins: [
     require('postcss-import'),
+    require('postcss-cssnext')({'browsers': 'last 2 versions'}),
     require('postcss-css-variables'),
     require('css-mqpacker'),
-    require('autoprefixer')({'browsers': 'last 2 versions'}),
-    require('cssnano')
+    require('cssnano')({ autoprefixer: false })
   ]
 }
