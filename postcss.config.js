6578c6df49b990a634f6e988f10a2555be7921f7
vipickering <hello@vincentp.me>
2018-05-09 08:50:52 +0100

    :lipstick: Giant face added.

--- postcss.config.js
+++ postcss.config.js
@@ -7,6 +7,7 @@ module.exports = {
             ]
         }),
     require('css-mqpacker'),
+    require('postcss-custom-media'),
     require('cssnano')({ autoprefixer: false }),
     require("postcss-reporter")({ clearReportedMessages: true })
   ]
