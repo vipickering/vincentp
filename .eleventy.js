55d28b62bdd01dd26de315dc7065fc2ca63fbec5
vipickering <hello@vincentp.me>
2020-02-29 11:50:28 +1300

    :construction: switched back to default _site

--- .eleventy.js
+++ .eleventy.js
@@ -44,7 +44,7 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.setBrowserSyncConfig({
         callbacks: {
             ready: function(err, bs) {
-                const content_404 = fs.readFileSync('dist/404.html');
+                const content_404 = fs.readFileSync('_site/404.html');
                 bs.addMiddleware("*", (req, res) => {
                     // Provides the 404 content without redirect.
                     res.write(content_404);
@@ -58,7 +58,7 @@ module.exports = function(eleventyConfig) {
     return {
         dir: {
             input: 'src',
-            output: 'dist',
+            output: '_site',
             layouts: '_layouts'
         },
         templateFormats: ['njk', 'md'],
