e1a5dcd5be6b259c064bddbac56992ea99ab0ad6
vipickering <hello@vincentp.me>
2024-11-24 17:53:52 +1300

    :sparkles: adding Bluesky verification

--- .eleventy.js
+++ .eleventy.js
@@ -22,6 +22,7 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy('src/browserconfig.xml');
     eleventyConfig.addPassthroughCopy('src/manifest.webmanifest');
     eleventyConfig.addPassthroughCopy('src/downloads');
+    eleventyConfig.addPassthroughCopy('src/.well-known');
 
     // Nunjucks Filters
     eleventyConfig.addFilter("limit", (array, limit) => array.slice(0, limit)); // limit array to first X items
